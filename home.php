aceb984df5e1be9298a51ce598931b36ee6ff92a
Syd <slynch2@uoregon.edu>
2017-12-05 13:54:48 -0800

    add homepage

---
+++ home.php
@@ -0,0 +1,14 @@
+<?php
+include('connection.php')
+
+
+?>
+
+<html>
+<head>
+  <title>Campus Housing System</title>
+</head>
+
+<body bgcolor="gray">
+
+<h4 align="center">Campus Housing System</h4>

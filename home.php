7760218733ecc8c73a2093f59499d2b5258810ba
Syd <slynch2@uoregon.edu>
2017-12-05 14:06:32 -0800

    table layout

--- home.php
+++ home.php
@@ -1,6 +1,9 @@
 <?php
 include('connection.php')
 
+#connect to the housing database
+$conn = mysqli_connect($server, $user, $pass, $dbname, $port)
+or die('Error connecting to MySQL server.');
 
 ?>
 
@@ -11,4 +14,32 @@ include('connection.php')
 
 <body bgcolor="gray">
 
-<h4 align="center">Campus Housing System</h4>
+<h1 align="center">Campus Housing System</h1>
+
+
+<table style="width:100%;">
+  <tr>
+    <th>Administrative Tools</th>
+    <th>Equipment Check-out</th>
+    <th>Key Checkout</th>
+    <th>Mail and Packages</th>
+  </tr>
+  <tr>
+    <td align="center">Roster</td>
+    <!-- <a href="..."> -->
+    <td align="center">Look-Up</td>
+    <td align="center">Check-In and Check-Out</td>
+  </tr>
+  <tr>
+    <td align="center">Loan</td>
+    <td align="center">Return</td>
+  </tr>
+  <tr>
+    <td align="center">Loan</td>
+    <td align="center">Return</td>
+  </tr>
+  <tr>
+    <td align="center">Log</td>
+    <td align="center">Deliver</td>
+  </tr>
+</table>

79dae048c44d40100357e602b44096ef42dc2183
Syd <slynch2@uoregon.edu>
2017-12-06 12:13:59 -0800

    updates!!

--- equipReturn.php
+++ equipReturn.php
@@ -59,7 +59,7 @@ Example (Basketball, 951000000)
 $query = 'SELECT item, student_id, CONCAT(first_name, " ", last_name) AS name, out_date, in_date
           FROM equipment_borrow
           JOIN resident USING (student_id)
-          ORDER BY in_date DESC';
+          ORDER BY in_date';
 
 $sql = $conn->prepare($query);
 $result = $sql->execute()

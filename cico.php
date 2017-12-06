2a3e083e31607ed84d91fb64ca82ab634119f50c
Syd <slynch2@uoregon.edu>
2017-12-05 18:56:53 -0800

    add cico.php

--- cico.php
+++ cico.php
@@ -16,6 +16,56 @@ or die('Error connecting to MySQL server.');
 
 <body bgcolor="white">
 
+<?php
+if (isset($_POST['id_in']) && isset($_POST['first_in']) && isset($_POST['last_in'])
+    && isset($_POST['group']) && isset($_POST['mailbox']) && isset($_POST['complex'])
+    && isset($_POST['hall']) && isset($_POST['room'])) {
+
+    }
+
+
+
+
+?>
+
+
+
+
+
+<p>
+Check-In a Resident (ID, First, Last, GroupCode, Gender, Mailbox)
+<p>
+<form action="lookup.php" method="POST" style="text-align: center;">
+
+<input type="text" name="id_in" placeholder="ID">
+<input type="text" name="first_in" placeholder="First Name">
+<input type="text" name="last_in" placeholder="Last Name">
+<input type="text" name="group" placeholder="Group Code">
+<input type="text" name="mailbox" placeholder="Mailbox">
+<br>
+Assign to an open room (listed below)
+<input type="text" name="complex" placeholder="Complex">
+<input type="text" name="hall" placeholder="Hall">
+<input type="text" name="room" placeholder="Room">
+<br>
+<input type="submit" value="submit">
+<input type="reset" value="erase">
+</form>
+
+<p>
+Check-Out a Resident (Name or ID)
+<form action="lookup.php" method="POST" style="text-align: center;">
+
+<input type="text" name="id_out" placeholder="ID">
+<input type="text" name="first_out" placeholder="First Name">
+<input type="text" name="last" placeholder="Last Name">
+<br>
+<input type="submit" value="submit">
+<input type="reset" value="erase">
+</form>
+
+
+
 <hr>
 <h4 align="center">Rooms without a person assigned</h4>
 
